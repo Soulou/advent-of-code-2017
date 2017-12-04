ec91d232505484fea64c526448b60144701d8a33
Soulou <leo@unbekandt.eu>
2017-12-04 09:58:37 +0100

    Add day 4

---
+++ 4/4-2.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+input = File.read("input")
+
+valids = 0
+
+input.each_line do |line|
+  line = line.split(" ").map{|word|
+    word.chars.sort.join
+  }.sort
+  len = line.length
+  ok = true
+
+  line.each_with_index do |word, i|
+    p = line[i-1] if i > 0
+    n = line[i+1] unless i == len - 1
+
+    if word == p || word == n
+      ok = false
+      break
+    end
+  end
+
+  valids += 1 if ok
+end
+
+puts valids
