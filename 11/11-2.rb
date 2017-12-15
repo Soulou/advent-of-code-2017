0d918c8a254731fdef5be825aed31bd303d346d5
Soulou <leo@unbekandt.eu>
2017-12-15 09:22:02 +0100

    Add day 11

---
+++ 11/11-2.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+input = File.read("input").split(",").map(&:strip)
+
+directions = {}
+maxdist = 0
+
+input.each do |i|
+  directions[i] ||= 0
+  directions[i] += 1
+
+  copy = {}
+  while copy != directions
+    copy = directions.clone
+    [["ne","sw"], ["n", "s"], ["nw", "se"],
+     ["ne", "s", "se"], ["nw", "s", "sw"],
+     ["se", "n", "ne"], ["sw", "n", "nw"],
+     ["sw", "se", "s"], ["nw", "ne", "n"]].each do |tuple|
+      d1 = directions[tuple[0]]
+      d2 = directions[tuple[1]]
+      if d1 && d1 > 0 &&
+         d2 && d2 > 0
+        min = [d1, d2].min
+        directions[tuple[0]] -= min
+        directions[tuple[1]] -= min
+        if tuple[2]
+          directions[tuple[2]] ||= 0
+          directions[tuple[2]] += min
+        end
+      end
+    end
+  end
+
+  dist = directions.values.sum
+  if dist >  maxdist
+    maxdist = dist
+  end
+end
+
+
+puts maxdist
+
